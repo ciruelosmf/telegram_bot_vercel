dc20c69ebab0daf095c5315fb47333254f7fcb7d
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-07-21 23:24:03 -0300

    as
    as

--- api/bot.js
+++ api/bot.js
@@ -1,13 +1,18 @@
 
 
+import { Bot, webhookCallback } from "grammy";
+console.log(1);
 
 
+const token = process.env.BOT_TOKEN;
+if (!token) throw new Error("BOT_TOKEN is unset");
 
-export default function handler(req, res) {
-    res.status(200).json({ message: 'Hello  .js!' })
-  }
+const bot = new Bot(token);
+
+export default webhookCallback(bot, "std/http");
 
 
+ 
 
 
 
