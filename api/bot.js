f5a01bc81c878569270694b89ca36c669946b876
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-07-21 21:20:37 -0300

    aa
    aa

--- api/bot.js
+++ api/bot.js
@@ -1,8 +1,14 @@
-import { Bot, webhookCallback } from "grammy";
-console.log(1);
-const token = process.env.BOT_TOKEN;
-if (!token) throw new Error("BOT_TOKEN is unset");
 
-const bot = new Bot(token);
 
-export default webhookCallback(bot, "std/http");
+
+
+
+export default function handler(req, res) {
+    res.status(200).json({ message: 'Hello from Next.js!' })
+  }
+
+
+
+
+
+ 
